d1bab92fba33f56dae6f1961278c6f9d83816326
iamargus95 <kamathsuraj95@gmail.com>
2021-11-30 01:38:16 +0530

    Added listRestaurants function within User_controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	conn "github.com/iamargus95/restaurant_rest_api_design/dbconn"
 	r "github.com/iamargus95/restaurant_rest_api_design/resources"
 	s "github.com/iamargus95/restaurant_rest_api_design/services"
+	"github.com/iamargus95/restaurant_rest_api_design/utils"
 )
 
 func Signup(ctx *gin.Context) {
@@ -21,8 +21,7 @@ func Signup(ctx *gin.Context) {
 		return
 	}
 
-	dbconn := conn.GetDB()
-	userUUID, serviceErr := s.Signup(dbconn, body)
+	userUUID, serviceErr := s.Signup(body)
 	if serviceErr != nil {
 		ctx.JSON(http.StatusForbidden, gin.H{
 			"error": serviceErr.Error(),
@@ -33,5 +32,22 @@ func Signup(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"GUID": userUUID,
+	})
+}
+
+func ListRestaurants(ctx *gin.Context) {
+
+	pagination := utils.GeneratePaginationFromRequest(ctx)
+	content, err := s.ListRestaurants(&pagination)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(http.StatusAccepted, gin.H{
+		"list of restaurants": content,
 	})
 }
