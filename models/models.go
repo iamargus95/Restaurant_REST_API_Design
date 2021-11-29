74f7ab28215c865b37903e25965f27a0fad57dc6
iamargus95 <kamathsuraj95@gmail.com>
2021-11-29 12:20:18 +0530

    Added user model with gorm.Model for db modelling

---
+++ models/models.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Users struct {
+	gorm.Model
+	Name  string
+	Email string
+	Phone string
+	Guid  uuid.UUID
+}
+
+func (t *Users) TableName() string {
+	return "users"
+}
+
+// type Restaurant struct {
+// 	gorm.Model
+// 	Name        string
+// 	Description string
+// 	Star_rating float32
+// 	Address     string
+// }
+
+// type Menu struct {
+// 	gorm.Model
+// 	Restaurant_id Restaurant
+// 	Name          string
+// 	Description   string
+// 	Menu_category int
+// 	Price         float32
+// }
