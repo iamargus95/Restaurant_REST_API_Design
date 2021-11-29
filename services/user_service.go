86f2fdb833cbabce0e500c49576efe95857e316e
iamargus95 <kamathsuraj95@gmail.com>
2021-11-30 01:39:31 +0530

    Added List Restaurants service with pagination

--- services/user_service.go
+++ services/user_service.go
@@ -2,12 +2,13 @@ package services
 
 import (
 	"github.com/google/uuid"
+	"github.com/iamargus95/restaurant_rest_api_design/dbconn"
+	conn "github.com/iamargus95/restaurant_rest_api_design/dbconn"
 	"github.com/iamargus95/restaurant_rest_api_design/models"
 	r "github.com/iamargus95/restaurant_rest_api_design/resources"
-	"gorm.io/gorm"
 )
 
-func Signup(db *gorm.DB, body r.SignupPayload) (uuid.UUID, error) {
+func Signup(body r.SignupPayload) (uuid.UUID, error) {
 
 	var nilUUID uuid.UUID
 	var newUser models.Users
@@ -20,6 +21,7 @@ func Signup(db *gorm.DB, body r.SignupPayload) (uuid.UUID, error) {
 		Guid:  userUUID,
 	}
 
+	db := conn.GetDB()
 	dbtranx := db.Create(&newUser)
 	if dbtranx.Error != nil {
 		return nilUUID, dbtranx.Error
@@ -27,4 +29,19 @@ func Signup(db *gorm.DB, body r.SignupPayload) (uuid.UUID, error) {
 
 	db.Save(&newUser)
 	return userUUID, nil
+}
+
+func ListRestaurants(pagination *models.Pagination) (*[]models.Restaurant, error) {
+
+	var listOfRestaurants []models.Restaurant
+	db := dbconn.GetDB()
+
+	offset := (pagination.Page - 1) * pagination.Limit
+	queryBuilder := db.Limit(pagination.Limit).Offset(offset).Order(pagination.Sort)
+	dbtranx := queryBuilder.Model(&models.Restaurant{}).Find(&listOfRestaurants)
+	if dbtranx.Error != nil {
+		return nil, dbtranx.Error
+	}
+
+	return &listOfRestaurants, nil
 }
